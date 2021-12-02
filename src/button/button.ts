4ffb89e025ffb15dc569ac52a5d73c22f7c313d9
Anthony Avila <anthony.p.avila@gmail.com>
2021-12-02 20:15:56 +0900

    make urbit bg color glow further out from button

--- src/button/button.ts
+++ src/button/button.ts
@@ -15,18 +15,21 @@ const isThreadParent = (tweet: Element) => {
 }
 
 function hoverButton(e) {
-  const action = e.target.closest(".urbit-visor-share-tweet-action");
+  const action = e.target.closest(".urbit-visor-share-tweet-action").querySelector("button");
   action.style.backgroundColor = "rgb(248, 250, 157, 0.5)";
-  action.style.borderRadius = "40%";
+  action.style.borderRadius = "50%";
   action.style.transitionDuration = "0.2s";
   action.style.transitionProperty = "background-color, box-shadow";
+  // action.style.position = 'absolute !important';
+  // action.style.top = '-5px';
+  // action.style.left = '-29px';
   const circle = e.target.closest(".urbit-visor-share-tweet-action").querySelector("circle");
   const path = e.target.closest(".urbit-visor-share-tweet-action").querySelector("path");
-  circle.style.stroke = "rgb(245, 203, 66)";
-  path.style.fill = "rgb(245, 203, 66)";
+  circle.style.stroke = "rgb(251, 189, 10)";
+  path.style.fill = "rgb(251, 189, 10)";
 }
 function unhoverButton(e) {
-  const action = e.target.closest(".urbit-visor-share-tweet-action");
+  const action = e.target.closest(".urbit-visor-share-tweet-action").querySelector("button");
   action.style.backgroundColor = "transparent";
   const circle = e.target.closest(".urbit-visor-share-tweet-action").querySelector("circle");
   const path = e.target.closest(".urbit-visor-share-tweet-action").querySelector("path");
@@ -58,9 +61,11 @@ const createVisorButton = (
   urbitButton.style.lineHeight = '1'
   urbitButton.style.outline = '0'
   urbitButton.style.padding = "0";
+  urbitButton.style.width = '38px';
+  urbitButton.style.height = '38px';
   urbitButton.style.position = 'relative';
   urbitButton.type = 'button'
-  urbitButton.innerHTML = `<svg class="urbit-visor-share-tweet-button-img" width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  urbitButton.innerHTML = `<svg class="urbit-visor-share-tweet-button-img" width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
      <circle cx="16" cy="16" r="13" fill="transparent" stroke="currentcolor" stroke-width="2"/>
      <path d="M22 14.0488H19.6306C19.4522 15.0976 18.9936 15.7317 18.1783 15.7317C16.7006 15.7317 15.8599 14 13.5669 14C11.3503 14 10.1783 15.3659 10 17.9756H12.3694C12.5478 16.9024 13.0064 16.2683 13.8471 16.2683C15.3248 16.2683 16.1146 18 18.4586 18C20.6242 18 21.8217 16.6341 22 14.0488Z" fill="currentcolor"/>
      </svg>
