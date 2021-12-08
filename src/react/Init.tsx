5d447309fa4e37c66c6c31b22b8c4192bca2a203
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-12-08 15:49:00 +0100

    Updated naming/text in various places

--- src/react/Init.tsx
+++ src/react/Init.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { urbitVisor } from "@dcspark/uv-core";
 import Welcome from "./Welcome";
 
-
-function Init(){
-    useEffect(()=>{
-        urbitVisor.registerName("Twitter Ext").then(res => console.log("Twitter extension registered with Urbit Visor"))
-    })
-    const [havePerms, setHavePerms] = useState(true);
-    const styles = {
-    
-    }
-    async function checkPerms(): Promise<void>{
-        urbitVisor.on("permissions_granted", [], (perms) => setHavePerms(true));
-        const res = await urbitVisor.authorizedPermissions();
-        setHavePerms(res.response.length > 0)
-    }
-    return (
-        <div style={styles}>
-          {!havePerms && <Welcome />}
-        </div>
-    )
+function Init() {
+  useEffect(() => {
+    urbitVisor
+      .registerName("Twitter UV")
+      .then((res) =>
+        console.log("Twitter extension registered with Urbit Visor")
+      );
+  });
+  const [havePerms, setHavePerms] = useState(true);
+  const styles = {};
+  async function checkPerms(): Promise<void> {
+    urbitVisor.on("permissions_granted", [], (perms) => setHavePerms(true));
+    const res = await urbitVisor.authorizedPermissions();
+    setHavePerms(res.response.length > 0);
+  }
+  return <div style={styles}>{!havePerms && <Welcome />}</div>;
 }
-
 
 export default <Init />;
--- src/react/Welcome.tsx
+++ src/react/Welcome.tsx
@@ -1,35 +1,45 @@
 import React from "react";
 import { urbitVisor } from "@dcspark/uv-core";
-import mainLogo from './icon128.png';
-
-
+import mainLogo from "./icon128.png";
 
 function Welcome() {
-    const styles = {
-        position: "fixed" as any,
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "500px",
-        height: "500px",
-        backgroundColor: "lightgrey",
-        padding: "1rem"
-    }
-    async function requestPerms(): Promise<void> {
-        await urbitVisor.requestPermissions(["shipName", "scry", "subscribe", "poke"]);
-    }
-    return (
-        <div id="uv-twitter-extension-welcome">
-            <div id="uv-twitter-extension-welcome-icon">
-                <img src={mainLogo} alt="" />
-            </div>
-            <h4>Welcome to the Urbit Visor Twitter Extension</h4>
-            <p>In order to use the extension you must first grant it permissions to scry, subscribe and read your Urbit ships' name.</p>
-            <p>Click on the button below to request permissions, then click on your Urbit Visor extension icon to grant them.</p>
-            <button id="uv-twitter-extension-welcome-button" onClick={requestPerms}>Request Permissions</button>
-        </div>
-    )
+  const styles = {
+    position: "fixed" as any,
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "500px",
+    height: "500px",
+    backgroundColor: "lightgrey",
+    padding: "1rem",
+  };
+  async function requestPerms(): Promise<void> {
+    await urbitVisor.requestPermissions([
+      "shipName",
+      "scry",
+      "subscribe",
+      "poke",
+    ]);
+  }
+  return (
+    <div id="uv-twitter-extension-welcome">
+      <div id="uv-twitter-extension-welcome-icon">
+        <img src={mainLogo} alt="" />
+      </div>
+      <h4>Welcome to the Twitter UV Extension</h4>
+      <p>
+        In order to use the extension you must first grant it permissions to
+        scry, subscribe and read your Urbit ships' name.
+      </p>
+      <p>
+        Click on the button below to request permissions, then click on your
+        Urbit Visor extension icon to grant them.
+      </p>
+      <button id="uv-twitter-extension-welcome-button" onClick={requestPerms}>
+        Request Permissions
+      </button>
+    </div>
+  );
 }
 
-
-export default Welcome
+export default Welcome;
