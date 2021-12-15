751188e8c65806648526fb036f146e355ea405fc
Anthony Avila <anthony.p.avila@gmail.com>
2021-12-15 19:37:31 +0900

    reformat Preview.tsx with Prettier

--- src/react/Preview.tsx
+++ src/react/Preview.tsx
@@ -2,105 +2,100 @@ import React from "react";
 import { Tweet, Poll } from "../api/client";
 import { pollOptions } from "../utils/parsing";
 
-
-
 const placeholder = {
-    author: {
-        name: "",
-        handle: "",
-        avatar: ""
-    },
-    pics: [],
-    time: "1d",
-    video: null,
-    text: "",
-    quote: null,
-    poll: null
-}
-
+  author: {
+    name: "",
+    handle: "",
+    avatar: "",
+  },
+  pics: [],
+  time: "1d",
+  video: null,
+  text: "",
+  quote: null,
+  poll: null,
+};
 
 interface PreviewProps {
-    tweet: Tweet
+  tweet: Tweet;
 }
 
-
-function Preview({tweet}: PreviewProps) {
-    console.log(tweet, "tweet at preview")
-    return (
-        <div id="tweet-preview">
-            <div id="tweet-preview-author">
-                <img id="avatar" src={tweet.author.avatar} alt="" />
-                <div id="tweet-author-name-wrapper">
-                <p id="tweet-author-name">{tweet.author.name}</p>
-                <p id="tweet-author-handle">@{tweet.author.handle}</p>
-                </div>
-                <p>Posted: {tweet.time}</p>
-            </div>
-            <div id="tweet-body">
-                <p id="tweet-text">{tweet.text}</p>
-                {!tweet.video && tweet.pics.length > 0 && <Pics pics={tweet.pics} />}
-                {tweet.video && <Video video={tweet.video} />}
-                {tweet.poll && <Poll poll={tweet.poll} />}
-                {tweet.quote && <Quote quote={tweet.quote} />}
-            </div>
+function Preview({ tweet }: PreviewProps) {
+  console.log(tweet, "tweet at preview");
+  return (
+    <div id="tweet-preview">
+      <div id="tweet-preview-author">
+        <img id="avatar" src={tweet.author.avatar} alt="" />
+        <div id="tweet-author-name-wrapper">
+          <p id="tweet-author-name">{tweet.author.name}</p>
+          <p id="tweet-author-handle">@{tweet.author.handle}</p>
         </div>
-    )
+        <p>Posted: {tweet.time}</p>
+      </div>
+      <div id="tweet-body">
+        <p id="tweet-text">{tweet.text}</p>
+        {!tweet.video && tweet.pics.length > 0 && <Pics pics={tweet.pics} />}
+        {tweet.video && <Video video={tweet.video} />}
+        {tweet.poll && <Poll poll={tweet.poll} />}
+        {tweet.quote && <Quote quote={tweet.quote} />}
+      </div>
+    </div>
+  );
 }
 
 function Quote({ quote }) {
-    return (
-        <div id="tweet-quote">
-            <div id="tweet-quote-author">
-                <p id="tweet-quote-author-name">{quote.author.name}</p>
-                <p id="tweet-quote-author-handle">@{quote.author.handle}</p>
-                <p id="tweet-quote-time">{quote.time}</p>
-            </div>
-            <div id="tweet-quote-body">
-                {quote.text}
-                {!quote.video && quote.pics.length > 0 && <Pics pics={quote.pics} />}
-                {quote.video && <Video video={quote.video} />}
-                {quote.poll && <Poll poll={quote.poll} />}
-            </div>
-        </div>
-    )
+  return (
+    <div id="tweet-quote">
+      <div id="tweet-quote-author">
+        <p id="tweet-quote-author-name">{quote.author.name}</p>
+        <p id="tweet-quote-author-handle">@{quote.author.handle}</p>
+        <p id="tweet-quote-time">{quote.time}</p>
+      </div>
+      <div id="tweet-quote-body">
+        {quote.text}
+        {!quote.video && quote.pics.length > 0 && <Pics pics={quote.pics} />}
+        {quote.video && <Video video={quote.video} />}
+        {quote.poll && <Poll poll={quote.poll} />}
+      </div>
+    </div>
+  );
 }
 
 function Pics({ pics }) {
-
-    return (
-        <div id="tweet-pictures">
-            {pics.map((pic, i) => {
-                return (
-                    <img key={i} className={`img-${pics.length}`} src={pic} alt="" />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div id="tweet-pictures">
+      {pics.map((pic, i) => {
+        return (
+          <img key={i} className={`img-${pics.length}`} src={pic} alt="" />
+        );
+      })}
+    </div>
+  );
 }
 
 function Video({ video }) {
-    return (
-        <div id="tweet-video">
-            <video preload="none" playsInline controls src={video}></video>
-        </div>
-    )
+  return (
+    <div id="tweet-video">
+      <video preload="none" playsInline controls src={video}></video>
+    </div>
+  );
 }
 
 function Poll({ poll }) {
-    const options = pollOptions(poll);
+  const options = pollOptions(poll);
 
-    return (
-        <div id="twitter-poll">
-            {options.map((opt, i) => {
-                return (
-                    <div key={i} className="twitter-poll-option">
-                        <p>{opt.label}</p>
-                        <p>{opt.count}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div id="twitter-poll">
+      {options.map((opt, i) => {
+        return (
+          <div key={i} className="twitter-poll-option">
+            <p>{opt.label}</p>
+            <p>{opt.count}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Preview;
