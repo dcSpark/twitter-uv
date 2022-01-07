1cbfc334bd3b533a6bdf93938ec216ae27d2b1e3
Anthony Avila <anthony.p.avila@gmail.com>
2022-01-08 01:31:25 +0900

    reformat parsing.ts

--- src/utils/parsing.ts
+++ src/utils/parsing.ts
@@ -1,70 +1,66 @@
-import { Tweet, Thread, Poll } from "../api/client";
+import { Tweet, Thread, Poll } from '../api/client';
 
 function tweetTitle(tweet: Tweet) {
-  const url = `https://twitter.com/${tweet.author.handle}/statuses/${tweet.index}`;
-  return `[Tweet by ${tweet.author.name} (@${tweet.author.handle})](${url}) Posted on ${tweet.time}
+    const url = `https://twitter.com/${tweet.author.handle}/statuses/${tweet.index}`;
+    return `[Tweet by ${tweet.author.name} (@${tweet.author.handle})](${url}) Posted on ${tweet.time}
     \n-------------------\n
   `;
 }
 function threadTitle(thread: Thread) {
-  const tweet = thread.parent;
-  const url = `https://twitter.com/${tweet.author.handle}/statuses/${tweet.index}`;
-  return `[Twitter Thread by ${tweet.author.name} (@${
-    tweet.author.handle
-  })](${url})\n
+    const tweet = thread.parent;
+    const url = `https://twitter.com/${tweet.author.handle}/statuses/${tweet.index}`;
+    return `[Twitter Thread by ${tweet.author.name} (@${tweet.author.handle})](${url})\n
 Length: ${thread.children.length + 1} Tweets\n
-Posted on ${
-    tweet.time
-  }\n\n\`-------------------------------------------------------\`\n\n`;
+Posted on ${tweet.time}\n\n\`-------------------------------------------------------\`\n\n`;
 }
 export function titleFromTweet(tweet: Tweet): string {
-  return `by @${tweet.author.handle} - ${tweet.time}`;
+    return `by @${tweet.author.handle} - ${tweet.time}`;
 }
 
 function tweetToMarkdown(tweet: Tweet) {
-  console.log(tweet, "parsing tweet");
-  const text = tweet.text + "\n\n";
-  const withMedia = tweet.video
-    ? text + `![](${tweet.video.href.replace(/\?.+/, "")})`
-    : tweet.pics.reduce((acc, picURL) => acc + `![](${picURL.href})`, text);
-  const withQuote = tweet.quote
-    ? withMedia +
-      "Quoting:\n" +
-      tweetToMarkdown(tweet.quote)
-        .split("\n")
-        .map((line) => `> ${line}`)
-        .join("\n")
-    : withMedia;
-  const withPoll = tweet.poll
-    ? withQuote +
-      pollOptions(tweet.poll).reduce(
-        (acc, opt) => acc + `${opt.label}: ${opt.count}\n`,
-        "\nPoll:\n"
-      ) +
-      "\n"
-    : withQuote;
-  return withPoll;
+    console.log(tweet, 'parsing tweet');
+    const text = tweet.text + '\n\n';
+    const withMedia = tweet.video
+        ? text + `![](${tweet.video.href.replace(/\?.+/, '')})`
+        : tweet.pics.reduce((acc, picURL) => acc + `![](${picURL.href})`, text);
+    const withQuote = tweet.quote
+        ? withMedia +
+          'Quoting:\n' +
+          tweetToMarkdown(tweet.quote)
+              .split('\n')
+              .map(line => `> ${line}`)
+              .join('\n')
+        : withMedia;
+    const withPoll = tweet.poll
+        ? withQuote +
+          pollOptions(tweet.poll).reduce(
+              (acc, opt) => acc + `${opt.label}: ${opt.count}\n`,
+              '\nPoll:\n'
+          ) +
+          '\n'
+        : withQuote;
+    return withPoll;
 }
 
 export function tweetToGraphStore(tweet: Tweet) {
-  return tweetTitle(tweet) + tweetToMarkdown(tweet);
+    return tweetTitle(tweet) + tweetToMarkdown(tweet);
 }
 
 export function threadToGraphStore(thread): string {
-  const title = threadTitle(thread);
-  const parent = tweetToMarkdown(thread.parent);
-  const children = thread.children.reduce((acc, tweet, index) => {
-    const text = tweetToMarkdown(tweet);
-    return acc + text;
-  }, "");
-  return title + parent + children;
+    const title = threadTitle(thread);
+    const parent = tweetToMarkdown(thread.parent);
+    const children = thread.children.reduce((acc, tweet, index) => {
+        const text = tweetToMarkdown(tweet);
+        return acc + text;
+    }, '');
+    return title + parent + children;
 }
 
 export function pollOptions(poll: Poll) {
-  const labels = Object.keys(poll).filter((key) => key.includes("label"));
-  const options = labels.map((label) => {
-    const count = label.split("_label")[0] + "_count";
-    return { label: poll[label], count: poll[count] };
-  });
-  return options;
+    const labels = Object.keys(poll).filter(key => key.includes('label'));
+    const options = labels.map(label => {
+        const count = label.split('_label')[0] + '_count';
+        return { label: poll[label], count: poll[count] };
+    });
+    return options;
 }
