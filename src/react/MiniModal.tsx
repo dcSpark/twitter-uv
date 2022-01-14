9b4b569d836dfac2666cab0217c3ef48687e519a
Anthony Avila <anthony.p.avila@gmail.com>
2022-01-14 23:24:07 +0900

    prettify minimodal.tsx

--- src/react/MiniModal.tsx
+++ src/react/MiniModal.tsx
@@ -1,32 +1,30 @@
-import React from "react";
+import React from 'react';
 
 interface UrbitChannel {
-    title: string;
-    type: "post" | "chat" | "link" | "publish" | null;
-    group: string;
-    ship: string;
-    name: string;
-};
-interface MiniModalProps{
-  type: "success" | "failure";
+  title: string;
+  type: 'post' | 'chat' | 'link' | 'publish' | null;
+  group: string;
+  ship: string;
+  name: string;
+}
+interface MiniModalProps {
+  type: 'success' | 'failure';
   channel?: UrbitChannel;
-};
+}
 
 export default function MiniModal(props: MiniModalProps) {
-//   const successMessage = `Tweet successfully shared on ${props.channel.ship}/${props.channel.group}/${props.channel.name}`
+  //   const successMessage = `Tweet successfully shared on ${props.channel.ship}/${props.channel.group}/${props.channel.name}`
   const successMessage = `Tweet shared succesfully`;
-  const failureMessage = "Error sharing the Tweet, please try again later";
-  const icon = props.type === "success" ? "✔️" : "❌"
-  const message = props.type === "success" ? successMessage : failureMessage;
-  
+  const failureMessage = 'Error sharing the Tweet, please try again later';
+  const icon = props.type === 'success' ? '✔️' : '❌';
+  const message = props.type === 'success' ? successMessage : failureMessage;
+
   return (
     <div id="uv-twitter-mini-modal">
-        <div id="uv-twitter-mini-modal-icon" className={`${props.type}-modal`}>
-         <p>{icon}</p>
-        </div>
-        <div id="uv-twitter-mini-modal-message">
-            {message}
-        </div>
+      <div id="uv-twitter-mini-modal-icon" className={`${props.type}-modal`}>
+        <p>{icon}</p>
+      </div>
+      <div id="uv-twitter-mini-modal-message">{message}</div>
     </div>
   );
 }
