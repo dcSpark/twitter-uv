7415c1cdcdae13dac7008a16f7caa5ef6f95732c
Anthony Avila <anthony.p.avila@gmail.com>
2022-01-29 18:57:43 +0900

    fix unroll button stylings and position

--- src/button/button.ts
+++ src/button/button.ts
@@ -43,29 +43,15 @@ function unhoverButton(e) {
 const createUnrollButton = () => {
   const unrollAction = document.createElement('div');
   unrollAction.className = 'urbit-visor-unroll-tweet-action';
-  unrollAction.style.display = 'flex';
-  unrollAction.style.width = '38px';
   unrollAction.setAttribute('role', 'button');
   unrollAction.setAttribute('tabindex', '0');
 
-  // Create the Twitter UV button
+  // Create the Twitter UV unroll button
   const urbitButton = document.createElement('button');
+  const urbitSvg = `<svg width="14" height="14" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="urbit-icon"><circle cx="16" cy="16" r="13" fill="white" stroke="currentcolor" stroke-width="2"></circle><path d="M22 14.0488H19.6306C19.4522 15.0976 18.9936 15.7317 18.1783 15.7317C16.7006 15.7317 15.8599 14 13.5669 14C11.3503 14 10.1783 15.3659 10 17.9756H12.3694C12.5478 16.9024 13.0064 16.2683 13.8471 16.2683C15.3248 16.2683 16.1146 18 18.4586 18C20.6242 18 21.8217 16.6341 22 14.0488Z" fill="black"></path></svg>`;
   urbitButton.className = 'urbit-visor-unroll-tweet-button';
-  urbitButton.style.background = 'transparent';
-  urbitButton.style.border = '0';
-  urbitButton.style.color = '#657786';
-  urbitButton.style.display = 'inline-block';
-  urbitButton.style.fontSize = '16px';
-  urbitButton.style.lineHeight = '1';
-  urbitButton.style.outline = '0';
-  urbitButton.style.padding = '0';
-  urbitButton.style.width = '24px';
-  urbitButton.style.height = '24px';
-  urbitButton.style.position = 'absolute';
-  urbitButton.style.top = '68px;';
-  urbitButton.style.left = '467px;';
   urbitButton.type = 'button';
-  urbitButton.innerHTML = `<p class="unroll-button">Unroll</p>`;
+  urbitButton.innerHTML = `${urbitSvg}<span>Unroll</span>`;
 
   unrollAction.append(urbitButton);
 
