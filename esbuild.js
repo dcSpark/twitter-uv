76a5d69844789fc9459d06792255eb88e4593cb2
Anthony Avila <anthony.p.avila@gmail.com>
2022-02-04 21:09:21 +0900

    test esbuild with module for sass

--- esbuild.js
+++ esbuild.js
@@ -1,16 +1,23 @@
-const { build } = require('esbuild');
+import { build } from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
+
+console.log('fire');
 
 build({
   entryPoints: ['./src/content.ts'],
   define: {
     'process.env.NODE_ENV': '"development"',
   },
-  outfile: './out.js',
+  outfile: './dist/dev.js',
   bundle: true,
   watch: true,
   external: ['react', 'react-dom'],
   plugins: [sassPlugin()],
+  loader: {
+    '.png': 'dataurl',
+  },
 })
   .then(console.log, console.error)
   .catch(e => console.error(e.message));
+
+// esbuild src/content.ts --outfile=dist/dev.js --bundle --watch --loader:.png=dataurl --loader:.scss=css
